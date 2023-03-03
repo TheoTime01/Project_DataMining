cc6f3a23e243655497d89db25774f7efcec06f45
TheotimeP <perricht@gmail.com>
2023-03-03 19:09:35 +0100

    color up

--- file_in_json.py
+++ file_in_json.py
@@ -46,57 +46,9 @@ for img_filename in os.listdir(img_dir):
             # add the metadata for this image to the dictionary of all metadata
             all_metadata[img_filename.split("\\")[-1]] = metadata
 
-            # Extraire la matrice de pixels
-            pixel_matrix = np.array(img)
-
-            # Extraire les valeurs R, G, B
-            r = pixel_matrix[:, :, 0].flatten()
-            g = pixel_matrix[:, :, 1].flatten()
-            b = pixel_matrix[:, :, 2].flatten()
-
-            # Calculer la couleur moyenne
-            mean_r = np.mean(r)
-            mean_g = np.mean(g)
-            mean_b = np.mean(b)
-
-            # Déterminer la couleur prédominante
-            if mean_r >= mean_g and mean_r >= mean_b:
-                main_color = "rouge"
-            elif mean_g >= mean_r and mean_g >= mean_b:
-                main_color = "vert"
-            else:
-                main_color = "bleu"
-
-            # create a dictionary of the colors for this image
-            color = {
-                "id": int((img_filename.split("\\")[-1]).split(".")[0]),
-                "couleur dominante": main_color,
-                "rouge": mean_r,
-                "vert": mean_g,
-                "bleu": mean_b
-            }
-            # add the color for this image to the dictionary of all colors
-            color_data[img_filename.split("\\")[-1]] = color
-
 
 # Add the the metadata to the JSON file
 with open('database.json', "w") as f:
     json.dump(all_metadata, f, indent=4)
-
-# Browse the JSON file and add the corresponding colors
-with open('database.json', 'r') as f:
-    data = json.load(f)
-
-for key in data:
-    for i in range(len(color_data)):
-        if data[key]["id"] == color_data[key]["id"]:
-            data[key]["color"] = color_data[key]
-
-# Remove the id from the color data and save it in the JSON file
-for key in data:
-    del data[key]["color"]["id"]
-
-with open('database.json', "w") as f:
-    json.dump(data, f, indent=4)
 
 
