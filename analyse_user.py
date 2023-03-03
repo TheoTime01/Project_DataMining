ac9b49f3aa71c83440fbbfc270b8e1bf7482c3a8
TheotimeP <perricht@gmail.com>
2023-03-03 20:39:28 +0100

    modèle trop précis

--- analyse_user.py
+++ analyse_user.py
@@ -21,35 +21,50 @@ def load(filename):
 def filter_images(images, color, legendary):
     filtered_images = []
     for image in images.values():
-        if image.get("color", {}).get("couleur dominante") == color:
-            tags = image.get("tags", {})
-            if bool(tags.get("Legendary")) == bool(legendary):
-                image["color"]["couleur dominante"] = color
-                filtered_images.append(image)
+        couleur = image["couleur dominante"]
+        legendaire = image["tags"]["Legendary"]
+        tags= image["tags"]
+        tags["color"]=color # ajouter color dans tags
+        # ajouter la lettre du debut du nom du pokemon dans tags
+        tags["first_letter"]=tags["Name"][0]
+        if couleur == color and not(legendaire^legendary):
+            filtered_images.append(tags)
     return filtered_images
-
 
 # Fonction de génération des préférences utilisateur
-def get_user_preferences(images):
-    color_t=["rouge","bleu","vert"]
+def get_user_preferences(images,color_t):
     legendary_t=[True,False]
-    color = choice(color_t)
-    legendary = choice(legendary_t)
+    color = color_t[randint(0, len(color_t)-1)]
+    legendary = legendary_t[randint(0, len(legendary_t)-1)]
     filtered_images = filter_images(images, color, legendary)
     return filtered_images
+
+# récuperation des nom couleurs dans database.json
+with open("database.json", "r") as f:
+    data=json.load(f)
+
+color_t=[]
+for i in data:
+    if data[i]["couleur dominante"] not in color_t:
+        color_t.append(data[i]["couleur dominante"])
 
 
 #simulation de l'utilisateur
 img,test=load("database.json")
-favorite_t=["Favorite","NotFavorite"]
+
+favorite_t=["favorite","notfavorite"]
 all_user={}
 
 for i in range(500):#500 utilisateurs
     result=[]
-    data=get_user_preferences(img)
+    data=get_user_preferences(img,color_t)
+    if len(data)==0:
+        continue
+
     for k in range(len(data)):
         result.append(favorite_t[randint(0, len(favorite_t)-1)])
     all_user[i]={'data':data,'result':result}
+    
 
 #sauvegarde des données
 with open("user.json", "w") as f:
